afede1d9bd91788464b59b6894a3c2b8437d0170
Oleksiy Tarlovskyy <tarlovskyy@gmail.com>
2020-08-22 19:24:46 +0100

    finished sb7 results coop

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 
+
 for i in 1 2 4 8 16;do
-  bash run-choice-co-op.sh sb7 $i TinySTM-igpu-cpu-persistent-dynamic-split-multithreaded wbetl
+  bash run-choice-co-op.sh sb7_20 $i TinySTM-igpu-cpu-persistent-dynamic-split-multithreaded wbetl
 done
 
 for i in 1 2 4 8 16;do
-  bash run-choice-co-op.sh sb7 $i TinySTM-igpu-cpu-persistent-dynamic-split-multithreaded wbetl-lsa
+  bash run-choice-co-op.sh sb7_20 $i TinySTM-igpu-cpu-persistent-dynamic-split-multithreaded wbetl-lsa
 done
